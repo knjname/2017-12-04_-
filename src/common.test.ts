a9db54450cc529abb8ab180a9c0a99b0750f7fd1
knjname <pc@knj.name>
2017-12-04 22:21:31 +0900

    Remove no longer required things

--- src/common.test.ts
+++
@@ -1,9 +0,0 @@
-import { makeDouble, dFormat } from "src/common";
-
-test("30 x 2 = 60", () => {
-    expect(makeDouble(30)).toBe(60)
-})
-
-test("ISO Date", () => {
-    expect(dFormat(new Date("2016-01-01"))).toBe("2016-01-01")
-})
--- types/global.d.ts
+++
@@ -1,5 +0,0 @@
-/** Global definitions for developement **/
-
-// for style loader
-declare module '*.scss';
-declare module '*.json';
